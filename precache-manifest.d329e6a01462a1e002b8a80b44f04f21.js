27d9640669b47bb2a8ae04df896ca3d578294793
izzudin26 <azidarmor26@gmail.com>
2020-07-16 02:45:20 +0700

    deploy

---
+++ precache-manifest.d329e6a01462a1e002b8a80b44f04f21.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e93103cb11253eafb5b",
+    "url": "/quran/css/app.ffba334a.css"
+  },
+  {
+    "revision": "54cca3eff6367e03b2ed",
+    "url": "/quran/css/chunk-533e2049.76874681.css"
+  },
+  {
+    "revision": "c14900fc868245988d93",
+    "url": "/quran/css/chunk-cc830334.4a438c06.css"
+  },
+  {
+    "revision": "c46c62a02f67bdac8649",
+    "url": "/quran/css/chunk-vendors.1cc28069.css"
+  },
+  {
+    "revision": "93d0644966b4000523beb8b67751201d",
+    "url": "/quran/img/smartphone-quran-min.93d06449.png"
+  },
+  {
+    "revision": "14f9cc4b4f7425dd0eb2bc758979f0e1",
+    "url": "/quran/index.html"
+  },
+  {
+    "revision": "8e93103cb11253eafb5b",
+    "url": "/quran/js/app.b785d97d.js"
+  },
+  {
+    "revision": "54cca3eff6367e03b2ed",
+    "url": "/quran/js/chunk-533e2049.c2923f1c.js"
+  },
+  {
+    "revision": "c14900fc868245988d93",
+    "url": "/quran/js/chunk-cc830334.4fb1831d.js"
+  },
+  {
+    "revision": "c46c62a02f67bdac8649",
+    "url": "/quran/js/chunk-vendors.445be66b.js"
+  },
+  {
+    "revision": "bbc6fe96242bf2ea72381c6a4aa9c86e",
+    "url": "/quran/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/quran/robots.txt"
+  }
+]);
